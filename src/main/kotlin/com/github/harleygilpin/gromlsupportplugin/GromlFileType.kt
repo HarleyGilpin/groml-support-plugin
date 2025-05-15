222743004cafcf10b7d36ddf52c66378dcdf6a4a
Harley <hg@harleygilpin.com>
2025-05-15 11:08:17 -0700

    Release 0.0.10 - add dark file icon

--- src/main/kotlin/com/github/harleygilpin/gromlsupportplugin/GromlFileType.kt
+++ src/main/kotlin/com/github/harleygilpin/gromlsupportplugin/GromlFileType.kt
@@ -8,14 +8,12 @@ object GromlFileType : LanguageFileType(GromlLanguage) {
     init {
         println(">> GromlFileType initialized")
     }
-
     override fun getName() = "Groml"
 
     override fun getDescription() = "Groml config file"
 
     override fun getDefaultExtension() = "groml"
 
-    override fun getIcon(): Icon = IconLoader.getIcon("/icons/file_icon.svg", javaClass)
-
+    override fun getIcon(): Icon = IconLoader.getIcon("/icons/groml.svg", javaClass)
 }
 
