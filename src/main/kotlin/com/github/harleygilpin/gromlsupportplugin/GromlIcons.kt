2bcb9efa8dacf4f7ca8e11f55e9e71d4d3a0b2fa
Harley <hg@harleygilpin.com>
2025-05-14 20:26:41 -0700

    Release 0.0.4 - Add GROML icon

--- src/main/kotlin/com/github/harleygilpin/gromlsupportplugin/GromlIcons.kt
+++
@@ -1,9 +0,0 @@
-package com.github.harleygilpin.gromlsupportplugin
-
-import com.intellij.openapi.util.IconLoader
-import javax.swing.Icon
-
-object GromlIcons {
-    @JvmField
-    val FILE: Icon = IconLoader.getIcon("/icons/groml.svg", javaClass)
-}
